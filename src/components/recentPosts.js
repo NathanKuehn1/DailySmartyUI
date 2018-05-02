9b943783894d0998f030d081d6a55b9c821d50c6
Nathan <linkingitup@outlook.com>
2018-05-02 12:11:07 -0600

    added recent posts

---
+++ src/components/recentPosts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const RecentPosts = () => {
+
+    return (
+        <div className="rescent-posts">
+            <div className="rescent-posts__wrapper">
+                <div className="rescent-posts__heading">Rescent Posts</div>
+                <ul className="rescent-posts__posts">
+                    <li>rescent post 0</li>
+                    <li>rescent post 1</li>
+                    <li>rescent post 2</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default RecentPosts;
